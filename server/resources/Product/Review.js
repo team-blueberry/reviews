5c18cc78490af0281b84c3a312b18d339d00d27b
Ari <ari.efron@gmail.com>
2018-12-19 17:23:38 -0500

    Modified review schema.

--- server/resources/Product/Review.js
+++ server/resources/Product/Review.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 let reviewSchema = mongoose.Schema({
+  reviewId: Number,
+  pageId: Number,
   stars: Number,
   username: String,
   text: String,
   title: String,
   verified: Boolean,
   helpful: Number,
-  images: Array
+  profilepicture: String,
+  date: String
 });
 
 var Review = mongoose.model('Review', reviewSchema);
