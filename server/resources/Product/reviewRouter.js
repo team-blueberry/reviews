7d717f50851c2d297f8ee42d27775c85c1276d5d
Ari <ari.efron@gmail.com>
2018-12-20 16:41:01 -0500

    Renames router file. Uses path.join to serve static assets instead of string concatenation.

--- server/resources/Product/reviewRouter.js
+++
@@ -1,7 +0,0 @@
-const reviewController = require('./reviewController.js');
-const reviewRouter = require('express').Router();
-
-reviewRouter.get('/listing', reviewController.retrieve);
-reviewRouter.get('/listing/:number', reviewController.retrieveOne);
-
-module.exports = reviewRouter;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const db = require('./db/index.js');
-const reviewRouter = require('./resources/Product/reviewRouter.js');
+const reviewsRouter = require('./resources/Product/reviewsRouter.js');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(__dirname + '../react-client/dist'));
-app.use('/', reviewRouter)
+app.use(express.static(path.join(__dirname, '../react-client/dist')));
+app.use('/', reviewsRouter)
 
 module.exports = app;
