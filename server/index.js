93364069664bb600f3aed8c532f206006671e0c8
Ari <ari.efron@gmail.com>
2018-12-20 16:18:43 -0500

    Fixes listener.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require('./server.js');
-const port = process.env.PORT || 3013;
+const port = 3013;
+
 
 app.listen(port, () => {
   console.log(`Reviews RESTful API listing on ${port}`)
