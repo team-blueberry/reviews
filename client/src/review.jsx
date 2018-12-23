2250a7b8835c6ca777adc3c50d7c606f1145cc1c
Ari <ari.efron@gmail.com>
2018-12-22 20:24:57 -0500

    Prettiers the file and replaces " with '

--- client/src/review.jsx
+++ client/src/review.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import axios from 'axios';
 
-
-
 class Review extends React.Component {
   constructor(props) {
     super(props);
@@ -13,27 +11,27 @@ class Review extends React.Component {
   }
 
   getReviews(listingNum) {
-    axios.get(`http://localhost:3013/listing/:${listingNum}`)
-    .then(({data}) => {
-      this.setState({reviews: data})
-    })
-
+    axios
+      .get(`http://localhost:3013/listing/:${listingNum}`)
+      .then(({ data }) => {
+        this.setState({ reviews: data });
+      });
   }
 
   componentDidMount() {
     var url = document.URL.substring(31);
-    this.getReviews(url);
+    this.getReviews(5);
   }
 
   render() {
     return (
       <React.Fragment>
-        
-        
+        {this.state.reviews.map(review => {
+          return <div>{review.title}</div>;
+        })}
       </React.Fragment>
     );
   }
 }
 
-
-ReactDom.render(<Review/>, document.getElementById('review'));
+ReactDom.render(<Review />, document.getElementById('review'));
