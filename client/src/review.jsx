1a4e4849db3ce1ccbe12216cf0510656c77357a5
Ari <ari.efron@gmail.com>
2018-12-29 22:55:50 -0500

    Adds function to calculate average review rating. Adds function to calculate number of each kind of need star (empty, half, full) to represent rating.

--- client/src/review.jsx
+++ client/src/review.jsx
@@ -2,34 +2,64 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import axios from 'axios';
 
+import AverageRating from './components/averageRating.jsx'
+
 class Review extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviews: []
+      reviews: [],
+      average: '',
+      neededStars: []
     };
   }
 
   getReviews(listingNum) {
-    axios
+    return axios
       .get(`http://localhost:3013/listing/:${listingNum}`)
       .then(({ data }) => {
         this.setState({ reviews: data });
       });
   }
 
+  getAverageStars() {
+    let average = (this.state.reviews.reduce((acc, review) => acc + review.stars, 0) / this.state.reviews.length).toFixed(2);
+    this.setState({average: average})
+  }
+
+  calculateNeededStars() {
+    let average = Number(this.state.average);
+    let filled = Math.floor(average);
+    let decimals = average - filled;
+    var halfStars = false;
+    let empty = 5 - filled;
+    if (decimals > .5) {
+      halfStars = true;
+      empty = empty - 1;
+    }
+    this.setState({
+      neededStars : [filled, halfStars, empty]
+    })
+  }
+
   componentDidMount() {
     var url = document.URL.substring(31);
-    this.getReviews(5);
+    this.getReviews(20)
+    .then(() => {
+      this.getAverageStars()
+    })
+    .then(() => {
+      this.calculateNeededStars()
+    })
   }
 
 
   render() {
     return (
       <React.Fragment>
-        {this.state.reviews.map(review => {
-          return <div>{review.title}</div>;
-        })}
+        <div id="reviewsMedly">
+          <AverageRating reviews={this.state.reviews} average={this.state.average} neededStars={this.state.neededStars}/>
+        </div>
       </React.Fragment>
     );
   }
