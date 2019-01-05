e4b8d5d578af5a6aac1d8309b979d6ba950de728
Ari <ari.efron@gmail.com>
2019-01-05 09:14:07 -0500

    tried using cors middleware

--- server/server.js
+++ server/server.js
@@ -4,17 +4,11 @@ const db = require('./db/index.js');
 const reviewsRouter = require('./resources/Product/reviewsRouter.js');
 const path = require('path');
 const compression = require('compression');
+const cors = require('cors')
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
- });
+app.use(cors())
 app.use(morgan('dev'));
 app.use(compression())
 app.use(express.json());
