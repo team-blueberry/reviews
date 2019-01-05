69456967ec17da5b854b7bd11abf57a7453aef50
Ari <ari.efron@gmail.com>
2019-01-05 08:58:36 -0500

    test

--- client/src/review.jsx
+++ client/src/review.jsx
@@ -31,7 +31,7 @@ class Review extends React.Component {
 
   getReviews(listingNum) {
     return axios
-      .get(`/http://ec2-13-59-251-226.us-east-2.compute.amazonaws.com/listing/${listingNum}`)
+      .get(`/http://ec2-13-59-251-226.us-east-2.compute.amazonaws.com:3013/listing/${listingNum}`)
       .then(({ data }) => {
         this.setState({ reviews: data });
       });
