91e25d82613a51c787ccd39aae30c373871f6ecd
Jared Ellison <jaredellison@gmail.com>
2019-01-11 15:59:34 -0500

    Split TSV creation into 7 parallel batches

--- db/seeding/fork-write.js
+++ db/seeding/fork-write.js
@@ -1,12 +1,9 @@
 const cp = require('child_process');
 
-// let child1 = cp.fork('./write-review-file.js', ['out1.tsv', 1000000,       0, 250000]);
-// let child2 = cp.fork('./write-review-file.js', ['out2.tsv', 1000000,  250000, 250000]);
-// let child3 = cp.fork('./write-review-file.js', ['out3.tsv', 1000000,  500000, 250000]);
-// let child4 = cp.fork('./write-review-file.js', ['out4.tsv', 1000000,  750000, 250000]);
-
-let child1 = cp.fork('./write-review-file.js', ['out1.tsv', 70000000,        0, 14000000]);
-let child2 = cp.fork('./write-review-file.js', ['out2.tsv', 70000000, 14000000, 14000000]);
-let child3 = cp.fork('./write-review-file.js', ['out3.tsv', 70000000, 28000000, 14000000]);
-let child4 = cp.fork('./write-review-file.js', ['out4.tsv', 70000000, 42000000, 14000000]);
-let child5 = cp.fork('./write-review-file.js', ['out5.tsv', 70000000, 56000000, 14000000]);
+let child1 = cp.fork('./write-review-file.js', ['out1.tsv', 70000000,        0, 10000000]);
+let child2 = cp.fork('./write-review-file.js', ['out2.tsv', 70000000, 10000000, 10000000]);
+let child3 = cp.fork('./write-review-file.js', ['out3.tsv', 70000000, 20000000, 10000000]);
+let child4 = cp.fork('./write-review-file.js', ['out4.tsv', 70000000, 30000000, 10000000]);
+let child5 = cp.fork('./write-review-file.js', ['out5.tsv', 70000000, 40000000, 10000000]);
+let child6 = cp.fork('./write-review-file.js', ['out6.tsv', 70000000, 50000000, 10000000]);
+let child7 = cp.fork('./write-review-file.js', ['out7.tsv', 70000000, 60000000, 10000000]);
