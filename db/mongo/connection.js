e906c5350d10ab40b82ad4d4b61b69f26ffc9b8c
Jared Ellison <jaredellison@gmail.com>
2019-01-12 12:22:53 -0500

    Revert to non promisified mongo connection

--- db/mongo/connection.js
+++ db/mongo/connection.js
@@ -10,15 +10,12 @@ const dbName = 'newReviews';
 const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
-module.exports.db = new Promise((resolve, reject) => {
-  client.connect(err => {
-    if (err) {
-      console.log('Error connecting to database');
-      reject();
-    }
-    console.log('Sucessfully connected to database');
-    resolve(client.db(dbName));
-  });
+client.connect((err) => {
+  if (err) {
+    console.log('Error connecting to database');
+    process.exit();
+  }
+  console.log("Sucessfully connected to database");
+  module.exports.db = client.db(dbName);
+  module.exports.client = client;
 });
-
-module.exports.client = client;
