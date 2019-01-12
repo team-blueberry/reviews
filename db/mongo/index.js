3d2388910daa4e6de1af2489c16f2ce7950d64b8
Jared Ellison <jaredellison@gmail.com>
2019-01-12 11:53:24 -0500

    Begin mongo db model script

---
+++ db/mongo/index.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'newReviews';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+client.connect((err) => {
+  if (err) {
+    console.log('Error connecting to database');
+    process.exit();
+  }
+  console.log("Sucessfully connected to database");
+  module.exports.db = client.db(dbName);
+  module.exports.client = client;
+});
+
+
+
+
+
