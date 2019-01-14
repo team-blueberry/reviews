76badda6fc7b5ab371daf1173baa24a2ca46b0bd
Jared Ellison <jaredellison@gmail.com>
2019-01-14 17:23:57 -0500

    Begin to modularize tsv writing script

--- db/seeding/write-tsv-in-parallel.js
+++ db/seeding/write-tsv-in-parallel.js
@@ -1,9 +1,15 @@
 const cp = require('child_process');
+const path = require('path');
 
-let child1 = cp.fork('./write-review-file.js', ['out1.tsv', 70000000,        0, 10000000]);
-let child2 = cp.fork('./write-review-file.js', ['out2.tsv', 70000000, 10000000, 10000000]);
-let child3 = cp.fork('./write-review-file.js', ['out3.tsv', 70000000, 20000000, 10000000]);
-let child4 = cp.fork('./write-review-file.js', ['out4.tsv', 70000000, 30000000, 10000000]);
-let child5 = cp.fork('./write-review-file.js', ['out5.tsv', 70000000, 40000000, 10000000]);
-let child6 = cp.fork('./write-review-file.js', ['out6.tsv', 70000000, 50000000, 10000000]);
-let child7 = cp.fork('./write-review-file.js', ['out7.tsv', 70000000, 60000000, 10000000]);
+const tsvWriter = path.join(__dirname, 'write-review-file.js');
+
+const PAGES = 1e6;
+const REVIEWS = 70e6;
+const BATCH_COUNT = 7;
+
+let reviewsPerBatch = REVIEWS / BATCH_COUNT;
+
+for (let i = 0; i < BATCH_COUNT; i++) {
+  // Create a temp TSV file
+  cp.fork(tsvWriter, [`out${i}.tsv`, PAGES, i * reviewsPerBatch, reviewsPerBatch]);
+}
