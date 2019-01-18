642ad2429e45c70fdc920e7942959dbac3b466eb
Jared Ellison <jaredellison@gmail.com>
2019-01-18 16:25:13 -0500

    Change API and client endpoints, remove unused dependencies and express router

--- server/index.js
+++ server/index.js
@@ -1,27 +1,29 @@
+// Dependencies
 const express = require('express');
 const morgan = require('morgan');
-const db = require('../db/postgres/index.js');
-const reviewsRouter = require('./resources/Product/reviewsRouter.js');
 const path = require('path');
 const compression = require('compression');
-const cors = require('cors')
 
+// Controller Functions
+const reviewController = require('./resources/Product/reviewController.js');
+
+// Configuration
 const port = process.env.PORT || 3013;
 
 const app = express();
 
-app.use(cors())
 app.use(morgan('dev'));
-app.use(compression())
-app.use(express.json());
+app.use(compression());
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/listing', reviewsRouter)
+// Client Endpoints
+app.use('/:id(\\d+)/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
+// API Endpoints
+app.use('/api/reviews/:number', reviewController.retrieveOne);
 
 module.exports = app;
 
-
 app.listen(port, () => {
-  console.log(`Reviews RESTful API listing on ${port}`)
-})
+  console.log(`Reviews component server listing on port# ${port}`);
+});
--- server/resources/Product/reviewsRouter.js
+++
@@ -1,6 +0,0 @@
-const reviewController = require('./reviewController.js');
-const reviewsRouter = require('express').Router();
-
-reviewsRouter.get('/:number', reviewController.retrieveOne);
-
-module.exports = reviewsRouter;
