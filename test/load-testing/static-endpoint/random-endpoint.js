cbf6b280247a62f0fe14c0a1d05368dc9424728d
Jared Ellison <jaredellison@gmail.com>
2019-01-24 11:45:05 -0500

    Request bundle.js file in static endpoint load tests

--- test/load-testing/static-endpoint/random-endpoint.js
+++ test/load-testing/static-endpoint/random-endpoint.js
@@ -2,6 +2,7 @@ module.exports.generateRandomEndpoint = (context, events, next) => {
   const MIN = 0;
   const MAX = 10000000;
   let target = `/${Math.floor(Math.random() * (MAX - MIN))}/`;
-  context.vars['endpoint'] = target;
+  context.vars['htmlEndpoint'] = target;
+  context.vars['bundleEndpoint'] = target + 'bundle.js';
   next();
 };
