87f9e45a57ecfca0ab866f76112579394430375f
Jared Ellison <jaredellison@gmail.com>
2019-02-05 10:53:38 -0500

    Use express-static-gzip to serve static files

--- server/index.js
+++ server/index.js
@@ -6,9 +6,22 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const compression = require('compression');
+const expressStaticGzip = require("express-static-gzip");
 
 // Controller Functions
 const reviewController = require('./reviewController.js');
+
+// Compression middleware filter
+const filterApiRequests = (req, res) => {
+
+  // Only compress requests to api
+  if (/\/api\/reviews\//.test(req.baseUrl)) {
+    return compression.filter(req, res)
+  }
+
+  // Otherwise expressStaticGzip should handle compression
+  return false
+}
 
 // Configuration
 const port = process.env.PORT || 3013;
@@ -16,11 +29,11 @@ const port = process.env.PORT || 3013;
 const app = express();
 
 app.use(morgan('dev'));
-app.use(compression());
+app.use(compression({filter: filterApiRequests}))
 
 // Client Endpoints
-app.use('/:id(\\d+)/', express.static(path.join(__dirname, '../client/dist')));
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/:id(\\d+)/', expressStaticGzip(path.join(__dirname, '../client/dist')));
+app.use('/', expressStaticGzip(path.join(__dirname, '../client/dist')));
 
 // API Endpoints
 app.use('/api/reviews/:number', reviewController.retrieveOne);
