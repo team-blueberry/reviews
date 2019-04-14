ed7533d84e96bd63bb613393778e871d9dd2c213
Jared Ellison <jaredellison@gmail.com>
2019-04-14 11:03:27 -0400

    Handle load test verification endpoint dynamically

--- server/index.js
+++ server/index.js
@@ -28,8 +28,8 @@ app.disable('x-powered-by');
 app.use(morgan('dev'));
 app.use(compression({ filter: filterApiRequests }));
 
-// HTTP Cache Control Constants
-const ONE_DAY = 86400000;
+// HTTP Cache Control Constants in ms
+const ONE_DAY = 1 * 24 * 60 * 60 * 1000;
 
 // Client Endpoints
 app.use(
@@ -39,7 +39,16 @@ app.use(
 app.use('/', expressStaticGzip(path.join(__dirname, '../client/dist'), { maxAge: ONE_DAY }));
 
 // API Endpoints
-app.use('/api/reviews/:number', reviewController.retrieveOne);
+app.get('/api/reviews/:number', reviewController.retrieveOne);
+
+// Load Test Verification
+// For verification, the load testing service loader.io sends requests to an
+// endpoint beginning with loaderio- that ends with a hash related to the
+// server's IP. This route responds to those requests with text that represents
+// that endpoint.
+app.get('/loaderio-(*)/', (req, res) => {
+  res.send(req.url.replace('/','').replace('.txt','').replace('.html',''));
+})
 
 app.listen(port, () => {
   console.log(`Reviews component server listing on port# ${port}`);
